2a0d0db581aa4aa341baf7c4992ab95d8c0632ee
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-10-18 10:06:55 +0700

    do homework

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("ggngngsadfsdfsdfna")
+let date_today = new Date();
+
+console.log(`The current day index of the week is: ${date_today.getDay()}`);
+
+console.log(`The current date of the week is: ${date_today.getDate()}`);
+
+let first_day_of_the_week = new Date(date_today.setDate(date_today.getDate() - date_today.getDay() ));
+
+let last_day_of_the_week = new Date(date_today.setDate(date_today.getDate() - date_today.getDay() + 6));
+
+console.log(`The first date of the week is: ${first_day_of_the_week}`); 
+
+console.log(`The last day of the week is: ${last_day_of_the_week}`);
