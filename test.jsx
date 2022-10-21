ec4d1be76840d9238d3ecd9ecc8c1c965a3705bf
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-10-21 20:56:04 +0700

    delete code

--- test.jsx
+++
@@ -1,5 +0,0 @@
-GLOBAL.document = new JSDOM(html).window.document
-const nodeList = document.querySelectorAll(".example");
-for (let i = 0; i < nodeList.length; i++) {
-    nodeList[i].style.backgroundColor = "red";
-}
